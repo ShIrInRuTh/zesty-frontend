d090fcf74c13e30228d4ac0078d0e7bd4f0e41cf
WilliamsSmithM <mw.smith.2024@computing.smu.edu.sg>
2025-10-10 20:15:50 +0800

    Fridge-Frontend

--- zesty/src/components/Landing.vue
+++ zesty/src/components/Landing.vue
@@ -9,7 +9,6 @@
 
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
       <nav>
         <RouterLink to="/" class="active" exact>Home</RouterLink>
@@ -28,6 +27,8 @@
           companion.
         </p>
         <RouterLink to="/recipes" class="explore-btn">Explore Recipes</RouterLink>
+        <!-- a hyperlink to fridge page for dev purposes only -->
+        <RouterLink to="/fridge" class="explore-btn">View Fridge [Test]</RouterLink> 
       </section>
     </main>
 
---
+++ zesty/src/views/FridgeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Fridge from '@/components/fridge.vue';
+import fridge from '@/components/fridge.vue';
+</script>
+
+<template>
+  <main>
+    <Fridge/>
+  </main>
+</template>
+
+
