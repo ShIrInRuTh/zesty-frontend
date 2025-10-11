60a9b3f7b562257ad68ac2fe4fcb9c474a9cd5cb
leynachee <leyna.chee.2024@computing.smu.edu.sg>
2025-10-12 00:30:12 +0800

    was told to commit before pulling

--- zesty/src/components/Landing.vue
+++ zesty/src/components/Landing.vue
@@ -91,6 +91,25 @@
           </section>
 
           <!-- EVENT CARDS -->
+          <div class="container">
+
+            <div class="row">
+
+              <div class="col">
+                <div class="card" style="width: 18rem;">
+                  <img src="../../public/logo.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                      content.</p>
+                    <a href="http://sunjun.site" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
 
         </main>
 
